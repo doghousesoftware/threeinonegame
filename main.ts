971402400ee5b2dde1fc087acd21772d24c5d3a5
bryan weinstein <bryanweinstein@outlook.com>
2020-10-14 17:29:26 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,88 @@
+let menuMode = 0
+let userChoice = 0
+let myRandom = 0
+input.onButtonPressed(Button.A, function () {
+    if (menuMode == 0) {
+        userChoice = 1
+        music.playMelody("C - - G - - - C5 ", 404)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (menuMode == 0) {
+        userChoice = 3
+        music.playMelody("C - - G - - - C5 ", 404)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (menuMode == 0) {
+        userChoice = 2
+        music.playMelody("C - - G - - - C5 ", 404)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    menuMode = 0
+    if (userChoice == 1) {
+        myRandom = randint(0, 1)
+        if (myRandom == 0) {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                . . # . .
+                # . . . #
+                . # # # .
+                `)
+            basic.pause(2000)
+            basic.clearScreen()
+        } else {
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . # . # .
+                . # . # .
+                # . . . #
+                `)
+            basic.pause(2000)
+            basic.clearScreen()
+        }
+    }
+    if (userChoice == 2) {
+        myRandom = randint(1, 3)
+        if (myRandom == 1) {
+            basic.showLeds(`
+                . . . # .
+                . # # # #
+                . # # # #
+                # # # # #
+                . # # # .
+                `)
+            basic.pause(2000)
+            basic.clearScreen()
+        } else if (myRandom == 2) {
+            basic.showLeds(`
+                # # # # #
+                # . . . #
+                # . . . #
+                # . . . #
+                # # # # #
+                `)
+            basic.pause(2000)
+            basic.clearScreen()
+        } else {
+            basic.showLeds(`
+                # # . . #
+                # # . # .
+                . . # . .
+                # # . # .
+                # # . . #
+                `)
+            basic.pause(2000)
+            basic.clearScreen()
+        }
+    }
+    if (userChoice == 3) {
+        myRandom = randint(1, 6)
+        basic.showNumber(myRandom)
+        basic.pause(2000)
+        basic.clearScreen()
+    }
+})
